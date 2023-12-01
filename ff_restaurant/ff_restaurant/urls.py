3796522ae088dc7d511d094804ea7788fb9584c6
almusfuad <almusfuad@gmail.com>
2023-12-02 03:44:23 +0600

    complete about us

--- ff_restaurant/ff_restaurant/urls.py
+++ ff_restaurant/ff_restaurant/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('meal/', include('meal.urls'), name = 'meal'),
+    path('about_us/', include('about_us.urls'), name = 'about_us'),
 ]
