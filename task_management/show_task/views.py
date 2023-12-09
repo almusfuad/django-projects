22552f8695333aa1e4557cd1f9104a6b6104c16a
almusfuad <almusfuad@gmail.com>
2023-12-09 06:37:21 +0600

    sort by date, and auto marking

--- task_management/show_task/views.py
+++ task_management/show_task/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from task.models import TaskModel
+from django.utils import timezone
 
 # Create your views here.
+def mark_tasks_as_completed():
+      overdue_tasks = TaskModel.objects.filter(task_assign_date__lt = timezone.now().date(), is_completed = False)
+      
+      for task in overdue_tasks:
+            task.is_completed = True
+            task.save()
+
+
 def show_task(request):
-      tasks = TaskModel.objects.all()
+      mark_tasks_as_completed()
+      
+      tasks = TaskModel.objects.order_by('task_assign_date').all()
       return render(request, 'show_task.html', {'tasks': tasks})
 
