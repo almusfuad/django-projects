254ba4f2844e8bfb266cacb69f01598c651a4563
almusfuad <almusfuad@gmail.com>
2023-12-20 04:31:56 +0600

    comment successfully

--- fantasy_car_point/home/models.py
+++ fantasy_car_point/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from car_details.models import CarModel
 
 # Create your models here.
+class Comment(models.Model):
+      car_details = models.ForeignKey(CarModel, on_delete = models.CASCADE, related_name = 'comments')
+      name = models.CharField(max_length = 30)
+      email = models.EmailField()
+      body = models.TextField()
+      created_on = models.DateTimeField(auto_now_add = True)
+      
+      
+      def __str__(self):
+            return f'Comments by {self.name}'
--- fantasy_car_point/home/views.py
+++ fantasy_car_point/home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, DetailView
 from car_details.models import Brand, CarModel
+from . import forms
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -25,9 +26,28 @@ class DetailPageView(DetailView):
     model = CarModel
     template_name = 'details.html'
     slug_url_kwarg = 'slug'
+    pk_url_kwarg = 'pk'
+    
+    def post(self, request, *args, **kwargs):
+        comment_form = forms.CommentForm(data = request.POST)
+        car_details = self.get_object()
+        
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit = False)
+            new_comment.car_details = car_details
+            new_comment.save()
+            return redirect('car_details',slug = car_details.slug)
+        return self.get(request, *args, **kwargs)
+    
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        car_details = self.object
+        comments = car_details.comments.all()
+        comment_form = forms.CommentForm()
         context['detail'] = self.object
+        context['comments'] = comments
+        context['comment_form'] = comment_form
+        
         
         return context
